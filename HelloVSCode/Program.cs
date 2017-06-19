70e6143adfc1299f20ef9c86424d27e56f44c6c7
KindRoach <kindroach@hotmail.com>
2017-06-19 13:46:32 +0800

    Hello GitHub!

--- HelloVSCode/Program.cs
+++ HelloVSCode/Program.cs
@@ -7,6 +7,7 @@ namespace HelloVSCode
         static void Main(string[] args)
         {
             Console.WriteLine("Hello VSCode!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
